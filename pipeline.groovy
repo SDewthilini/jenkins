e896fc59bfd6cb14fb45d031cae58de913d8f807
Sandalu Dewthilini <150440179+SDewthilini@users.noreply.github.com>
2025-01-18 15:26:59 +0530

    second commit

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                sh 'mkdir -p /mnt/c/Users/ismad/devops/test'
+                //sh 'mkdir -p /mnt/c/Users/ismad/devops/test'
             }
         }
     }
